b430a122115a80a90699073f8e7911cb8c9b59c1
viniciusjn <viniciusaqpsjn@gmail.com>
2025-02-20 16:50:14 -0300

    primeiro teste (scroll)

---
+++ codeflix-client/__tests__/hooks/useScroll.test.tsx
@@ -0,0 +1,24 @@
+import { useScroll } from "@/app/hooks/useScroll";
+import { describe, it, expect } from "@jest/globals";
+import { renderHook, act } from '@testing-library/react';
+
+describe('useScroll', () => {
+    it('should respond to scroll events', () => {
+        const { result } = renderHook (() => useScroll());
+
+        act (() => {
+            global.window.scrollY = 100;
+            global.window.dispatchEvent(new Event('scroll'));
+        });
+
+        expect(result.current).toBe(true);
+
+        act (() => {
+            global.window.scrollY = 0;
+            global.window.dispatchEvent(new Event('scroll'));
+        });
+
+        expect(result.current).toBe(false);
+
+    });
+});
