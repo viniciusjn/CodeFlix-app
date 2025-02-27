56dc071dbf61d2fd2251ae6373fb7bcc9e8cec9d
viniciusjn <viniciusaqpsjn@gmail.com>
2025-02-26 23:09:04 -0300

    configuração no banner

--- codeflix-client/src/app/components/Banner.tsx
+++ codeflix-client/src/app/components/Banner.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 
 export function Banner({ movie }: { movie: Movie }) {
     return (
-        <div className="mb-10">
-            <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+        <div className="mb-10 lg:mb-20">
+            <div className="flex flex-col space-y-4 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
                 <div className="absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-[black]">
                         
                     <video
@@ -36,7 +36,7 @@ export function Banner({ movie }: { movie: Movie }) {
 
             </div>
 
-            <div className="flex space-x-2">
+            <div className="flex space-x-3">
                 <Link
                     href={`/watch/${movie.id}`}
                     className='flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
